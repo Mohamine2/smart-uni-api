9d511eda929492dd2fcf875ee9b122ccda352646
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2026-08-11 21:01:41 +0200

    Configure settings for DRF and JWT

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,15 @@ else:
         }
     }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ]
+}
+
 # --- FILES (Static & Media) ---
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -61,6 +70,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'residence_connectee'
 ]
 
