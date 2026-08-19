fe4eed5816c743e551f6b437ffc592b99dd85dcb
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2026-08-19 16:30:57 +0200

    (feat): Configure Swagger

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,15 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Smart Uni API',
+    'DESCRIPTION': 'Interactive endpoint documentation',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
 
 SIMPLE_JWT = {
@@ -81,6 +89,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',
     'residence_connectee'
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,5 +17,14 @@ urlpatterns = [
     path("api/token/refresh/",
          TokenRefreshView.as_view(),
          name="token_refresh"
-    )
+    ),
+
+    # Raw OpenAPI Scheme (YAML/JSON)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI Interface
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # Alternative ReDoc (static documentation-oriented view)
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
