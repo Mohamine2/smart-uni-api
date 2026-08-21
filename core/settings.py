b9eef3f8a9c91ca2cc55fa181b18f1b3ca102499
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2026-08-21 20:06:43 +0200

    fix(docker): inject missing secret key and environment variables in compose

--- core/settings.py
+++ core/settings.py
@@ -74,7 +74,6 @@ SIMPLE_JWT = {
 # --- FILES (Static & Media) ---
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
